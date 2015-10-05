9b1a98f14f4cb29f68158acbcf19880b3f00e7a8
ArtemSidun <tomas1772@gmail.com>
2015-10-05 21:19:40 +0300

    AfterLesson Work

--- js/main.js
+++ js/main.js
@@ -1,18 +1,29 @@
-var app = function() {
+var app = (function() {
     var searchHub = {};
     var myForm = document.querySelector("#propCross");
     var placeInput = document.querySelector("#propLocation");
     var resultBlock = document.querySelector(".propResult");
-    var placeName;
+ 
     var perPage;
     var searhUrl;
     var currentListPos = 0;
     var resultContent = "";
 
+    var placeName = (function() {
+      var _value = "";
+      return {
+        get: function() {
+          return _value;
+        },
+         set: function(val) {
+          _value = val;
+        }
+      }
+   })();
 
-    function getplaceName() {
-      return document.querySelector("#propLocation").value;
-    } 
+    // function getplaceName() {
+    //   return document.querySelector("#propLocation").value;
+    // } 
     function getitemPerPage() {
       var selectItem = document.querySelector("#placesPerPage");
       return selectItem.options[selectItem.selectedIndex].text;
@@ -20,22 +31,34 @@ var app = function() {
    
     function eventHandler(event) {
       event.preventDefault();
-      if (( event.keyCode === 13) || (event == "submit")) {
-        placeName = getplaceName();
+        placeName.set(document.querySelector("#propLocation").value);
         perPage = getitemPerPage();
         recieveData();
-
-      };
     }
     function recieveData() {
       $.ajax({
-        url: "http://api.nestoria.co.uk/api?country=uk&pretty=1&action=search_listings&encoding=json&listing_type=buy&page=1&number_of_results=" + perPage + "&place_name=" + placeName,
+        url: "http://api.nestoria.co.uk/api?country=uk&pretty=1&action=search_listings&encoding=json&listing_type=buy&page=1&number_of_results=" + perPage + "&place_name=" + placeName.get(),
         method: 'GET',
         timeout: 5000,
         dataType: "jsonp",
         success: function(data) {
-        searchHub[placeName] = data;
-        makeResultList(searchHub[placeName].response.listings.slice(currentListPos, currentListPos + perPage - 1));
+          switch(data.response.application_response_code) {
+            case "100":
+            case "101":
+            case "110":
+              searchHub[placeName.get()] = data;
+              makeResultList(searchHub[placeName.get()].response.listings.slice(currentListPos, currentListPos + perPage - 1));
+            break;
+            case "200":
+            case "202":
+              searchHub[placeName.get()] = data;
+              makeResultList(searchHub[placeName.get()].response.listings.slice(currentListPos, currentListPos + perPage - 1));
+              showLoactionList(data.response);
+            break;
+            default:
+              showErrorList(data.response);
+          }
+        
         }
         // error: function() {}
     });
@@ -61,8 +84,7 @@ var app = function() {
 
 
     myForm.addEventListener("submit", eventHandler);
-    placeInput.addEventListener("keyup", eventHandler);
   
- };
+ });
  app();
 
